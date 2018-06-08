859c9ad2e91754ec489a3402c61434a667c9c9cd
Jake <jake@treepangolin.com>
2018-06-08 00:09:05 -0400

    Made loading a little less ugly

--- global.js
+++ global.js
@@ -1,7 +1,6 @@
-window.onload = chooseImage;
-
 var images = ["awoo.png", "game.png", "hide.png"]
 var image = new Image()
+var faLogo = new Image()
 
 function chooseImage() {
     var random = Math.floor(Math.random() * images.length)
@@ -21,3 +20,15 @@ function chooseImage() {
     localStorage.setItem("lastMascotShown", random)
     image.style.width = "320px"
 }
+
+faLogo.onload = function() {
+    var links = document.getElementById("links")
+    var faHref = document.getElementById("fa-href")
+
+    setTimeout(chooseImage, 150)
+    links.classList.add("animated", "fadeIn")
+    links.classList.remove("invisible")
+    faHref.appendChild(faLogo)
+}
+
+faLogo.src = "social_fa.png"

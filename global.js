f7e3e8a217eb36be95889ebfe3a28df52f6edbb8
Jake <jake@treepangolin.com>
2018-06-07 23:47:56 -0400

    Improved randomizing

--- global.js
+++ global.js
@@ -7,11 +7,16 @@ function chooseImage() {
     var random = Math.floor(Math.random() * images.length)
     var mascot = document.getElementById("mascot")
 
+    while (random == localStorage.getItem("lastMascotShown")) {
+        random = Math.floor(Math.random() * images.length)
+    }
+
     image.onload = function() {
         mascot.appendChild(image)
-        image.classList.add("animated", "fadeInDown")
+        image.classList.add("animated", "zoomInDown")
     }
 
     image.src = images[random]
+    localStorage.setItem("lastMascotShown", random)
     image.style.width = "320px"
 }

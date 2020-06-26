0aa8cc4157bb7f9f82ee850fcb47c67f322de398
Jacob Koenen <1koenenjacob@gmail.com>
2020-06-25 20:04:48 -0400

    Changes

--- js/global.js
+++ js/global.js
@@ -1,26 +1,34 @@
-var images = ["awoo.png", "confused.png", "heart.png", "itsk.png", "snug.png"];
-var image = new Image();
+// var images = ["awoo.png", "confused.png", "heart.png", "itsk.png", "snug.png"];
+// var image = new Image();
 
 window.onload = function() {
-    var random = Math.floor(Math.random() * images.length);
-    var mascot = document.getElementById("mascot");
+    fadeLinks();
+    fadeTitle();
 
-    while (random == localStorage.getItem("lastMascotShown")) {
-        random = Math.floor(Math.random() * images.length);
-    }
+    // var random = Math.floor(Math.random() * images.length);
+    // var mascot = document.getElementById("mascot");
 
-    image.onload = function() {
-        fadeLinks();
-        mascot.appendChild(image);
-        mascot.removeChild(document.getElementById("placeholder"));
-        image.classList.add("animated", "fadeInDownBig", "mascot");
-    };
+    // while (random == localStorage.getItem("lastMascotShown")) {
+    //     random = Math.floor(Math.random() * images.length);
+    // }
 
-    image.src = "img/stickers/" + images[random];
-    localStorage.setItem("lastMascotShown", random);
+    // image.onload = function() {
+    //     fadeLinks();
+    //     mascot.appendChild(image);
+    //     mascot.removeChild(document.getElementById("placeholder"));
+    //     image.classList.add("animated", "fadeInDownBig", "mascot");
+    // };
+
+    // image.src = "img/stickers/" + images[random];
+    // localStorage.setItem("lastMascotShown", random);
 };
 
 function fadeLinks() {
     links.classList.add("animated", "fadeInUp");
     links.classList.remove("invisible");
+}
+
+function fadeTitle() {
+    hello.classList.add("animated", "fadeInDownBig")
+    hello.classList.remove("invisible");
 }
